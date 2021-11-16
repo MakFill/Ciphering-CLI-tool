6febe02baa74a6a7b9eb7dda25b0ff78cf4cad42
MakFil <makfil1177@gmail.com>
2021-11-16 19:30:31 +0200

    fix: add errorHandler import to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { pipeline } from 'stream';
-import { stdout } from './helpers/index.js';
+import { stdout, errorHandler } from './helpers/index.js';
 import { input_stream, output_stream, getCiphersStreams } from './mainStreams/index.js';
 
 pipeline(input_stream, ...getCiphersStreams(), output_stream, (err) => {
