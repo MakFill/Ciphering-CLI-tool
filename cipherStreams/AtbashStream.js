9cd7c1efa7717ed84c6267152f933149da1a4771
MakFil <makfil1177@gmail.com>
2021-11-17 20:35:09 +0200

    refactor: Atbash, Encode, Decode streams

--- cipherStreams/AtbashStream.js
+++ cipherStreams/AtbashStream.js
@@ -7,24 +7,28 @@ import {
   ALPHABET_LENGTH,
 } from './cipherVariables.js';
 
+export function implementAtbash(chunk) {
+  let resultString = '';
+  const data = chunk.toString().split('');
+  data.forEach((letter) => {
+    let code = letter.charCodeAt();
+    if (code >= FIRST_LETTER_LOWER_CASE && code <= LAST_LETTER_LOWER_CASE) {
+      let alphabetCode = code - FIRST_LETTER_LOWER_CASE + 1;
+      code = ALPHABET_LENGTH - alphabetCode + 1 + (FIRST_LETTER_LOWER_CASE - 1);
+    }
+    if (code >= FIRST_LETTER_UPPER_CASE && code <= LAST_LETTER_UPPER_CASE) {
+      let alphabetCode = code - FIRST_LETTER_UPPER_CASE + 1;
+      code = ALPHABET_LENGTH - alphabetCode + 1 + (FIRST_LETTER_UPPER_CASE - 1);
+    }
+
+    resultString += String.fromCharCode(code);
+  });
+
+  return resultString;
+}
+
 export class AtbashStream extends Transform {
   _transform(chunk, encoding, callback) {
-    let resultString = '';
-    const data = chunk.toString().split('');
-    data.forEach((letter) => {
-      let code = letter.charCodeAt();
-      if (code >= FIRST_LETTER_LOWER_CASE && code <= LAST_LETTER_LOWER_CASE) {
-        let alphabetCode = code - FIRST_LETTER_LOWER_CASE + 1;
-        code = ALPHABET_LENGTH - alphabetCode + 1 + (FIRST_LETTER_LOWER_CASE - 1);
-      }
-      if (code >= FIRST_LETTER_UPPER_CASE && code <= LAST_LETTER_UPPER_CASE) {
-        let alphabetCode = code - FIRST_LETTER_UPPER_CASE + 1;
-        code = ALPHABET_LENGTH - alphabetCode + 1 + (FIRST_LETTER_UPPER_CASE - 1);
-      }
-
-      resultString += String.fromCharCode(code);
-    });
-
-    callback(null, resultString);
+    callback(null, implementAtbash(chunk));
   }
 }
