a1687ede0dc73c01230e86a8bbd04786fb70bf24
MakFil <makfil1177@gmail.com>
2021-11-20 21:35:33 +0200

    refactor: change description for third test in cliTool.test

--- __tests__/cliTool.test.js
+++ __tests__/cliTool.test.js
@@ -2,7 +2,7 @@ import { findIndex } from '../helpers/index.js';
 
 describe('Function findindex', () => {
   afterEach(() => {
-    process.argv.pop();
+    process.argv.length = 2;
   });
 
   test('Is function with the correct index returns an argument', () => {
@@ -11,17 +11,16 @@ describe('Function findindex', () => {
     expect(mockFn()).toBe(process.argv.length - 1);
   });
 
-  test('Is function with the uncorrect index returns false', () => {
+  test('Is function with the incorrect index returns false', () => {
     process.argv.push('-d');
     const mockFn = jest.fn().mockImplementationOnce(() => findIndex('-c'));
     expect(mockFn()).toBeFalsy();
   });
 
-  test('Is function with the uncorrect index returns false', () => {
+  test('Is function with the duplicated flag options returns custom error', () => {
     process.argv.push('-c');
     process.argv.push('-c');
     const mockFn = jest.fn().mockImplementationOnce(() => findIndex('-c'));
     expect(mockFn).toThrowError('Error: Duplicated flag options');
-    process.argv.pop();
   });
 });
